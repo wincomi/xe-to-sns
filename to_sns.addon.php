eb3c83bc5c10a10bdd3fb1c10463a09a13abb3df
COMI <admin@wincomi.com>
2017-09-22 21:45:48 +0900

    코드 정리

--- to_sns.addon.php
+++ to_sns.addon.php
@@ -10,7 +10,10 @@ if(!defined("__XE__"))
 if($addon_info->only_admin != "N")
 {
 	$logged_info = Context::get("logged_info");
-	if($logged_info->is_admin!="Y") return; // 관리자가 아니면 return
+	
+	if($logged_info->is_admin != "Y") {
+		return;
+	}
 }
 
 if(Context::get("act") == "procBoardInsertDocument" && $called_position == "after_module_proc" && $this->toBool())
@@ -26,29 +29,6 @@ if(Context::get("act") == "procBoardInsertDocument" && $called_position == "afte
 	
 	$document_title = $oDocument->variables["title"];
 	$document_url = getFullUrl("", "mid", Context::get('mid'), "document_srl", $oDocument->variables["document_srl"]);
-		
-	// 글자 수 자르기
-	function strcut_utf8($str, $len, $checkmb = false, $tail = "") {
-		preg_match_all("/[\xE0-\xFF][\x80-\xFF]{2}|./", $str, $match); // target for BMP
-		
-		$m = $match[0];
-		$slen = strlen($str); // length of source string
-		$tlen = strlen($tail); // length of tail string
-		$mlen = count($m); // length of matched characters
-		
-		if ($slen <= $len) return $str;
-		if (!$checkmb && $mlen <= $len) return $str;
-		
-		$ret = array();
-		$count = 0;
-		for ($i=0; $i < $len; $i++) {
-		    $count += ($checkmb && strlen($m[$i]) > 1)?2:1;
-		    if ($count + $tlen > $len) break;
-		    $ret[] = $m[$i];
-		}
-		
-		return join("", $ret).$tail;
-	}
 	
 	if(mb_strlen($document_title, "UTF-8") + strlen($document_url) >= 139)
 	{
@@ -66,6 +46,29 @@ if(Context::get("act") == "procBoardInsertDocument" && $called_position == "afte
 		$tt_oauth = new TwitterOAuth($addon_info->tt_consumer_key, $addon_info->tt_consumer_secret, $addon_info->tt_access_token, $addon_info->tt_access_token_secret);
 		$tt_oauth->post("statuses/update", array("status" => $message));
 	}
+}
+
+// 글자 수 자르기
+function strcut_utf8($str, $len, $checkmb = false, $tail = "") {
+	preg_match_all("/[\xE0-\xFF][\x80-\xFF]{2}|./", $str, $match); // target for BMP
+	
+	$m = $match[0];
+	$slen = strlen($str); // length of source string
+	$tlen = strlen($tail); // length of tail string
+	$mlen = count($m); // length of matched characters
+	
+	if ($slen <= $len) return $str;
+	if (!$checkmb && $mlen <= $len) return $str;
+	
+	$ret = array();
+	$count = 0;
+	for ($i=0; $i < $len; $i++) {
+	    $count += ($checkmb && strlen($m[$i]) > 1)?2:1;
+	    if ($count + $tlen > $len) break;
+	    $ret[] = $m[$i];
+	}
+	
+	return join("", $ret).$tail;
 }
 
 /* End of file to_sns.addon.php */
